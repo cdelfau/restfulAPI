b0cdc7f89cc0f887062b923716aa169f6fd8e3be
James Wang <jameswang9909@hotmail.com>
2016-12-06 13:33:59 -0500

    Add app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return ""
+
+if __name__ == "__main__":
+
+    # Generate and store secret key if it doesn't already exist
+    with open(".secret_key", "a+b") as f:
+        secret_key = f.read()
+        if not secret_key:
+            secret_key = os.urandom(64)
+            f.write(secret_key)
+            f.flush()
+        app.secret_key = secret_key
+
+    app.debug = True
+    app.run()
